040181ccf533bc63e639e1f1eb5640640ffd1589
Giridhar1404 <137368552+Giridhar1404@users.noreply.github.com>
2025-03-18 14:50:37 +0530

    Add files via upload

---
+++ palprime.java
@@ -0,0 +1,60 @@
+package core;
+
+import java.util.*;
+
+public class palprime {
+	public static void main(String[]args)
+	{
+		int prime=0;
+		int palindrome=0;
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter a number : ");
+	int n=sc.nextInt();
+	int p=n;
+	int d=0;
+	int rev=0;
+	int count=0;
+	for(int i=1;i<=n;i++)
+	{
+		if(n%i==0)
+		{
+		    count++;		
+		}
+	}
+	if(count==2)
+	{
+		prime=1;
+	}
+	else
+	{
+		prime=0;
+	}
+	
+	while(p!=0)
+	{
+		d=p%10;
+		rev=rev*10+d;
+		p=p/10;
+			
+	}
+	p=n;
+	if(rev==p)
+	{
+		palindrome=1;
+	}
+	else
+	{
+		palindrome=0;
+	}
+	
+	if(prime==1 && palindrome==1)
+	{
+		System.out.println("Its a Palprime number");
+	}
+	else
+	{
+		System.out.println("Its not a Palprime number");
+	}
+	}
+
+}
