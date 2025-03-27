75cae19b83ee8816044fd304bdd7016d169f92b2
Giridhar1404 <137368552+Giridhar1404@users.noreply.github.com>
2025-03-27 12:33:44 +0530

    Update customer.java

--- customer.java
+++ customer.java
@@ -5,10 +5,12 @@ public class customer {
 	int cid;
 	String cname;
 	String v;
-	public customer(int cid, String cname, String v) {
+	int bill;
+	public customer(int cid, String cname, String v,int bill) {
 		this.cid = cid;
 		this.cname = cname;
 		this.v = v;
+		this.bill=bill;
 	}
 	
 
